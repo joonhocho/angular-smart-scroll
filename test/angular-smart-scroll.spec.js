dc24d13acde11ccde5ca0743ea01b4237a87d28a
Joon Ho Cho <joon@42go.com>
2014-02-13 19:08:55 -0800

    update test

--- test/angular-smart-scroll.spec.js
+++ test/angular-smart-scroll.spec.js
@@ -5,22 +5,25 @@ describe('Module: jun.smartScroll', function () {
 
 	beforeEach(module('jun.smartScroll'));
 
+	var $rootScope,
+		$compile,
+		$window,
+		$document,
+		$timeout,
+		el,
+		scope;
+
+	beforeEach(inject(function (_$rootScope_, _$compile_, _$window_, _$document_, _$timeout_) {
+		$rootScope = _$rootScope_;
+		$compile = _$compile_;
+		$window = _$window_;
+		$document = _$document_;
+		$timeout = _$timeout_;
+	}));
+
 	describe('basic functionality', function () {
-		var $rootScope,
-			$compile,
-			$window,
-			$document,
-			$timeout,
-			el,
-			scope;
 
-		beforeEach(inject(function (_$rootScope_, _$compile_, _$window_, _$document_, _$timeout_) {
-			$rootScope = _$rootScope_;
-			$compile = _$compile_;
-			$window = _$window_;
-			$document = _$document_;
-			$timeout = _$timeout_;
-
+		beforeEach(function () {
 			el = angular.element('<div smart-scroll></div>');
 			el.css({
 				height: '1000px'
@@ -30,7 +33,7 @@ describe('Module: jun.smartScroll', function () {
 			$compile(el)(scope);
 
 			scope.$apply();
-		}));
+		});
 
 		afterEach(function () {
 			el.remove();
